2f99080ac75aa22158e7bba0d2af0e797b3c4198
Hinrich Riedel <hinrich.riedel@joblift.de>
2020-03-29 09:59:26 +0200

    transform image

--- src/server.ts
+++ src/server.ts
@@ -13,23 +13,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  // Transform image endpoint
+  // takes an image url and sends back a transfored version of that image.
+  app.get('/filteredimage', async (req: express.Request, res: express.Response) => {
+    const { image_url } = req.query;
+    if(!image_url) {
+      // send bad request if image_url is missing
+      res.statusCode = 400;
+      res.send("image_url query param must be present.");
+    }
+    // transform image
+    let filepath: string;
+    try {
+      filepath = await filterImageFromURL(image_url);  
+    }
+    catch(err) {
+      const { filepath, message} = err;
+      res.statusCode = 500;
+      res.send(`sorry, something went wrong. reason: ${message}`);
+      if(filepath) deleteLocalFiles([filepath]);
+      return;
+    }
 
-  /**************************************************************************** */
-
-  //! END @TODO1
+    // send image
+    res.sendFile(filepath, () => {
+      // cleanup
+      deleteLocalFiles([filepath])
+    });
+    
+  })
   
   // Root Endpoint
   // Displays a simple message to the user
